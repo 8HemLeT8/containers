813e6672efea8ad163a156ce41ecca8188dab9ca
8hemlet8 <barel987@gmail.com>
2019-05-08 16:27:52 +0300

    0.01

--- Demo.cpp
+++ Demo.cpp
@@ -6,12 +6,12 @@
  */
 
 
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 #include "range.hpp"
 #include "chain.hpp"
-#include "zip.hpp"
+#include "zip.hpp" 
 #include "product.hpp"
 #include "powerset.hpp"
 using namespace itertools;
